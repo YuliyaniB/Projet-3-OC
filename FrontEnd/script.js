0dc9665454922665f4b716af0980cb64e8c296da
Julien Brunel <julien.brunel.dev@gmail.com>
2024-06-05 01:41:25 +0200

    fetch and display categories

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,6 +1,7 @@
 // Récupération de l'élément du DOM qui accueillera les projets
 const gallery = document.querySelector(".gallery")
-
+// Récupération de l'élément du DOM qui accueillera les boutons filtres
+const filters = document.getElementById("filter-container")
 
 
 // Récuperation dynamique des projets avec l'API
@@ -34,7 +35,29 @@ async function displayWorks () {
 displayWorks()     
 // creer deux methodes : fetch et generer gallery pour le works et categories.
 
+// Récuperation dynamique des catégories avec l'API
+async function fetchCategories() {
+    try {
+    const getData = await fetch("http://localhost:5678/api/categories");
+    return await getData.json();
+    } catch (error) {
+        console.error("Impossible de contacter le serveur");
+    }
+}
 
+// Affichage dynamique des catégories
+async function displayCategories () {
+    const categories = await fetchCategories();
+    categories.forEach((category) => {
+        // Création des boutons filtres
+        const filtresElement = document.createElement("button");
+        filtresElement.textContent = category.name;
+        filtresElement.id = category.id;
+        // On rattache les balises button à la div filtres
+        filters.appendChild(filtresElement);
+    });    
+}
+displayCategories();
 
 
 
