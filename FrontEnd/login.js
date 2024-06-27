faa38c8bb386e33331aa27b65eb2f7f7f07606cd
Julien Brunel <julien.brunel.dev@gmail.com>
2024-06-27 14:47:01 +0200

    fonction delete

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,85 +1,33 @@
-/*
-// Variables globales pour le login
-const form = document.querySelector("form")
-const email = document.getElementById("email")
-const password = document.getElementById("password")
-const messageError = document.querySelector("#login p")
+function getLogged() {
+    const logInfo = {
+        password: document.getElementById("password"),
+        email: document.getElementById("email"),
+        submit: document.getElementById("connect"),
+    };
+    addEventListener("submit", (e) => {
+        e.preventDefault();
 
-
-
-
-async function fetchLogin() {
-    try {
-        const reponse = await fetch("http://localhost:5678/api/users/login", {
+        fetch("http://localhost:5678/api/users/login", {
             method: "POST",
             headers: {
-                "Accept": "application.json",
-                "Content-type": "application.json"
+                Accept: "application/json",
+                "Content-Type": "application/json",
             },
-            body : JSON.stringify()
-        });
-        const postData = await reponse.json();
-    } catch (error) {
-        console.error("Impossible de contacter le serveur");
-    }
-}
-
-
-
-async function login() {
-    const users = await fetchLogin();
-    console.log(users);
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-        const userEmail = email.value;
-        const userPwd = password.value;
-        console.log(userEmail, userPwd);
-
+            body: JSON.stringify({
+                email: logInfo.email.value,
+                password: logInfo.password.value,
+            }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                if (data.message || data.error) {
+                    alert("Erreur dans l\'identifiant ou le mot de passe");
+                } else {
+                    sessionStorage.setItem("Token", data.token);
+                    window.location.replace("index.html");
+                }
+            })
     })
 }
-login()
-
-console.log("Hello Worlds");
-*/
-
-// si je lance la fonction le serveur plante
-
-// l'element html email 
-
-
-const element = {
-    password: document.getElementById("password"),
-    email: document.getElementById("email"),
-    submit: document.getElementById("connect"),
-   
-};
-
-
-addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    fetch("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-        email: element.email.value,
-        password: element.password.value,
-        }),
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            if (data.message || data.error) {
-                alert("Erreur dans l\'identifiant ou le mot de passe");
-            } else {
-                //sessionStorage.setItem("isConnected", JSON.stringify(true));//
-                sessionStorage.setItem("Token", data.token);
-                window.location.replace("index.html");
-            }
-        })
-});
-
-// possibilite de changer le texte sur le bouton login au lieur de rajouter un logout
+getLogged()
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -8,20 +8,23 @@ const filters = document.getElementById("filter-container")
 async function fetchWorks() {
     try {
         const getData = await fetch("http://localhost:5678/api/works");
-        return await getData.json();
+        return getData.json();
     } catch (error) {
         console.error("Impossible de contacter le serveur");
     }
 }
 
 // Affichage dynamique des projets
-async function displayWorks() {
-    const works = await fetchWorks();
-    works.forEach(work => {
-        createWorks(work)
-    });
+function displayWorks() {
+    const gallery = document.querySelector(".gallery")
+    gallery.innerHTML = ""
+    fetchWorks()
+    .then((data) => {
+        data.forEach((work) => {
+            createWorks(work)
+        })
+    })
 }
-
 
 function createWorks(work) {
     // Creation balise figure
@@ -102,39 +105,11 @@ async function filterCategories() {
 // mettre tout les appels à fonction en bas de page
 // creer super fonction HOF en haut " init ui" => recupere les works avec les 3 fonctions 
 //pour eviter les fetch api a chaque debut de fonction.
-// Appeler l'API le moins possible
+
 
 displayWorks();
 displayCategories();
 filterCategories();
-
-/*
-function loggedElements() {
-    const login = document.getElementById("login-li")
-    const filter = document.getElementById("filter-container")
-    
-    if (JSON.parse(sessionStorage.getItem("isConnected"))) {
-        login.style.display = "none"
-        filter.style.display = "none"
-    } else {
-
-    }
-    }
-loggedElements()
-
-
-function logout() {
-    const logout = document.getElementById("logout")
-    logoutStatus.addEventListener("click", (event) => {
-        event.preventDefault();
-        sessionStorage.removeItem("Token");
-        sessionStorage.removeItem("isConnected");
-        window.location.replace("login.html");
-    });    
-}
-logout()
-*/
-
 
 // Affichage de la page admin lorsque le user est connecté
 function logged() {
@@ -168,3 +143,206 @@ function logout() {
     })
 }
 logout();
+
+
+
+
+
+// Affichage de la Première modale au click sur modifier ou mode Edition
+function displayModals() {
+    const modify = document.getElementById("modify-gallery");
+    const edit = document.getElementById("edition-mode");
+    const modal = document.getElementById("modify-container");
+    modify.addEventListener("click", () => {
+        modal.style.display = "flex";
+    })
+    edit.addEventListener("click", () => {
+        modal.style.display = "flex";
+    })
+    /*
+    modal.addEventListener("click", (e) => {
+        if (e.target.idName = "modify-container") {
+            modal.style.display = "none";
+        }
+    })*/
+}
+displayModals();
+
+// Accès à la deuxieme partie de la modale au click de l'input "Ajouter une photo"
+function displaySecondModal() {
+    const addButton = document.getElementById("add");
+    const modify = document.getElementById("modify-container");
+    const addWorks = document.getElementById("edit-container");
+    addButton.addEventListener("click", () => {
+        modify.style.display = "none";
+        addWorks.style.display = "flex";
+    })
+}
+displaySecondModal();
+
+// Retour à la Première Modale au click de la flèche
+function BackToFirstModal() {
+    const arrow = document.querySelector(".fa-arrow-left");
+    const modify = document.getElementById("modify-container");
+    const addWorks = document.getElementById("edit-container");
+    arrow.addEventListener("click", () => {
+        //Supréssion de la prewiew a clik sur retour dans la modale
+        /*inputFile.value = "";
+        previewImage.style.display = "none";*/
+        modify.style.display = "flex";
+        addWorks.style.display = "none";
+    });
+}
+BackToFirstModal();
+
+// Fermeture des modales au click de la croix
+function closeModals() {
+    const firstXmark = document.querySelector("#modify-container .fa-xmark");
+    const SecondXmark = document.querySelector("#edit-container .fa-xmark");
+    const deleteWorks = document.getElementById("modify-container");
+    const addWorks = document.getElementById("edit-container");
+    firstXmark.addEventListener("click", () => {
+        deleteWorks.style.display = "none";
+    })
+    SecondXmark.addEventListener("click", () => {
+        addWorks.style.display = "none";
+    })
+}
+closeModals();
+
+
+// Affichage dynamique des projets sur la premiere modale
+function WorksModal() {
+    gallery.innerHTML = ""
+    fetchWorks()
+    .then((works) => {
+        works.forEach((work) => {
+            createWorksModal(work);
+        })
+        deleteWorks()
+    })
+}
+WorksModal();
+
+function createWorksModal(work) {
+    // Creation balise figure
+    const figure = document.createElement("figure");
+    // Creation des contenus projets
+    const img = document.createElement("img");
+    img.src = work.imageUrl;
+    img.alt = work.title
+    // Création span
+    const span = document.createElement("span");
+    // Création icone poubelle
+    const trash = document.createElement("i");
+    trash.classList.add("fa-solid", "fa-trash-can");
+    trash.id = work.id;
+    // On rattache la balise figure à la div HandleWorks
+    const gallery = document.querySelector("#handle-works");
+    gallery.appendChild(figure);
+    // On rattache l'image et le span à la balise figure
+    figure.appendChild(img);
+    figure.appendChild(span);
+    // On rattache l'icone poubelle au span
+    span.appendChild(trash);
+}
+
+
+// Suppréssion des projets dans la première modale et dans la gallery
+function deleteWorks() {
+    const token = window.sessionStorage.getItem("Token");
+    console.log(token);
+    const allTrash = document.querySelectorAll(".fa-trash-can")
+    console.log(allTrash);
+    allTrash.forEach(trash => {
+        trash.addEventListener("click", (e) => {
+            const id = trash.id
+            const init = {
+                method: "DELETE",
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    Accept: `*/*`,
+                    "Content-Type": "application/json",
+                }
+            }
+            fetch(`http://localhost:5678/api/works/${id}`, init)
+                .then((response) => {
+                    if (!response.ok) {
+                        console.log("didn't work");
+                    }
+                    return response.json()                 
+                })
+                .then((data) => {
+                    console.log("worked" + data);
+                    WorksModal()
+                    displayWorks()
+                })
+                
+        })
+    })
+}
+
+
+
+
+
+// Previsualisation de l'image dans la deuxieme modale
+function previewImage() {
+    const icon = document.querySelector("#add-image .fa-image")
+    const label = document.querySelector("#add-image label")
+    const input = document.querySelector("#add-image input")
+    const img = document.querySelector("#add-image img")
+    const p = document.querySelector("#add-image p")
+    input.addEventListener("change", () => {
+        const file = input.files[0]
+        if (file) {
+            const reader = new FileReader()
+            reader.onload = function (e) {
+                img.src = e.target.result
+                img.style.display = "flex"
+                icon.style.display = "none"
+                label.style.display = "none"
+                p.style.display = "none"
+            }
+            reader.readAsDataURL(file)
+        } else {
+            img.style.display = "none"
+        }
+    })
+}
+previewImage()
+
+
+// Gerer les categories dans l'input category de la deuxieme modale
+async function fetchCategories() {
+    try {
+        const getData = await fetch("http://localhost:5678/api/categories");
+        return await getData.json();
+    } catch (error) {
+        console.error("Impossible de contacter le serveur");
+    }
+}
+
+async function categorySelect() {
+    const select = document.querySelector("form select");
+    const categorys = await fetchCategories();
+    categorys.forEach((category) => {
+        const option = document.createElement("option");
+        option.textContent = category.name;
+        option.value = category.id;
+        select.appendChild(option);
+    });
+}
+categorySelect()
+
+
+
+
+// Post works
+
+
+
+// Rajouter fonction size max img
+
+// faire un fichier service.js avec les fetch 
+// Postman pour m'entrainer au appel backend
