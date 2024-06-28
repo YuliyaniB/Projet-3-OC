d8c9353d14097f7098658a57d74a16a6149250b8
Julien Brunel <julien.brunel.dev@gmail.com>
2024-06-28 19:55:50 +0200

    login.js version finale

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -22,6 +22,44 @@ function getLogged() {
             .then((data) => {
                 console.log(data);
                 if (data.message || data.error) {
+                    const error = document.getElementById("error-login")
+                    error.textContent = "Erreur dans l\'identifiant ou le mot de passe."
+
+                } else {
+                    sessionStorage.setItem("Token", data.token);
+                    window.location.replace("index.html");
+                }
+            })
+    })
+}
+getLogged();
+
+
+/*
+function getLogged() {
+    const logInfo = {
+        password: document.getElementById("password"),
+        email: document.getElementById("email"),
+        submit: document.getElementById("connect"),
+    };
+    addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email: logInfo.email.value,
+                password: logInfo.password.value,
+            }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                if (data.message || data.error) {
                     alert("Erreur dans l\'identifiant ou le mot de passe");
                 } else {
                     sessionStorage.setItem("Token", data.token);
@@ -30,4 +68,4 @@ function getLogged() {
             })
     })
 }
-getLogged()
+getLogged();*/
