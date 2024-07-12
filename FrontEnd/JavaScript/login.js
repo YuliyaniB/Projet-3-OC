806441d028ad4dd8c001ec030c1e746b4865fdf2
Julien Brunel <julien.brunel.dev@gmail.com>
2024-07-12 10:34:01 +0200

    version finale

---
+++ FrontEnd/JavaScript/login.js
@@ -0,0 +1,106 @@
+// Permettre la connection grace au Token
+function getLogged() {
+    const logInfo = {
+        password: document.getElementById("password"),
+        email: document.getElementById("email"),
+        submit: document.getElementById("connect"),
+    };
+    addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email: logInfo.email.value,
+                password: logInfo.password.value,
+            }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                if (data.message || data.error) {
+                    const error = document.getElementById("error-login")
+                    error.textContent = "Erreur dans l\'identifiant ou le mot de passe."
+                    password.value = "";
+
+                } else {
+                    sessionStorage.setItem("Token", data.token);
+                    window.location.replace("index.html");
+                }
+            })
+    })
+}
+getLogged();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+// Permettre la connection grace au Token
+function getLogged() {
+    const logInfo = {
+        password: document.getElementById("password"),
+        email: document.getElementById("email"),
+        submit: document.getElementById("connect"),
+    };
+    addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        fetch("http://localhost:5678/api/users/login", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email: logInfo.email.value,
+                password: logInfo.password.value,
+            }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                if (data.message || data.error) {
+                    const error = document.getElementById("error-login")
+                    error.textContent = "Erreur dans l\'identifiant ou le mot de passe."
+                    password.value = "";
+
+                } else {
+                    sessionStorage.setItem("Token", data.token);
+                    window.location.replace("index.html");
+                }
+            })
+    })
+}
+getLogged();
+*/
+
